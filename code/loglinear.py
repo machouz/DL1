utilis: corriger bug dans one-hot loglinear : update softmax to get appropriate dimensions init params uniform numbers betwenn -1 and 1 train_loglin: develop training

code/loglinear.py
@@ -4,6 +4,7 @@ from utils import *
 STUDENT = {'name': 'YOUR NAME',
            'ID': 'YOUR ID NUMBER'}
 
+CATEGORIES = len(L2I)
 
 def softmax(x):
     """
@@ -11,9 +12,9 @@ def softmax(x):
     x: a n-dim vector (numpy array)
     returns: an n-dim vector (numpy array) of softmax values
     """
-    x = np.exp(x) - np.exp(max(x))
-    x /= np.sum(x)
-    return x
+    shiftx = x - np.max(x)
+    exps = np.exp(shiftx)
+    return exps / np.sum(exps)
 
 
 def classifier_output(x, params):
@@ -21,8 +22,9 @@ def classifier_output(x, params):
     Return the output layer (class probabilities) 
     of a log-linear classifier with given params on input x.
     """
+
     W, b = params
-    probs = softmax(W.dot(x) + b)
+    probs = softmax(np.dot(x, W) + b)
     return probs
 
 
@@ -54,9 +56,9 @@ def loss_and_gradients(x, y, params):
     y_pred = classifier_output(x, params)
     loss = -np.log(y_pred[y])
 
-    y_one_hot = one_hot_vector(y_pred)
-    gW = np.outer(y_pred - y_one_hot, x)
-    gB = y_pred - y_one_hot
+    y_one_hot = one_hot_vector(y, CATEGORIES)
+    gW = np.outer(x, y_pred - y_one_hot)
+    gb = y_pred - y_one_hot
     return loss, [gW, gb]
 
 
@@ -65,12 +67,18 @@ def create_classifier(in_dim, out_dim):
     returns the parameters (W,b) for a log-linear classifier
     with input dimension in_dim and output dimension out_dim.
     """
-    W = np.zeros((in_dim, out_dim))
-    b = np.zeros(out_dim)
+
+    #eps = np.sqrt(6.0 / (in_dim + out_dim))
+    shape = (in_dim, out_dim)
+    W = np.random.uniform(-1, 1, shape)
+    b = np.random.uniform(-1, 1, out_dim)
     return [W, b]
 
 
 if __name__ == '__main__':
+
+
+
     # Sanity checks for softmax. If these fail, your softmax is definitely wrong.
     # If these pass, it may or may not be correct.
     test1 = softmax(np.array([1, 2]))
@@ -85,6 +93,7 @@ if __name__ == '__main__':
     print test3
     assert np.amax(np.fabs(test3 - np.array([0.73105858, 0.26894142]))) <= 1e-6
 
+    '''
     # Sanity checks. If these fail, your gradient calculation is definitely wrong.
     # If they pass, it is likely, but not certainly, correct.
     from grad_check import gradient_check
@@ -93,13 +102,13 @@ if __name__ == '__main__':
 
 
     def _loss_and_W_grad(W):
-        global b
+        global W
         loss, grads = loss_and_gradients([1, 2, 3], 0, [W, b])
         return loss, grads[0]
 
 
     def _loss_and_b_grad(b):
-        global W
+        global b
         loss, grads = loss_and_gradients([1, 2, 3], 0, [W, b])
         return loss, grads[1]
 
@@ -109,3 +118,5 @@ if __name__ == '__main__':
         b = np.random.randn(b.shape[0])
         gradient_check(_loss_and_b_grad, b)
         gradient_check(_loss_and_W_grad, W)
+        
+    '''

code/train_loglin.py
@@ -7,22 +7,33 @@ STUDENT={'name': 'YOUR NAME',
          'ID': 'YOUR ID NUMBER'}
 
 VOCAB_SIZE = len(vocab)
+CATEGORIES = len(L2I)
+num_iterations = 10
+learning_rate = 0.01
 
 def feats_to_vec(features):
     vec = np.zeros(VOCAB_SIZE)
+    features = map(F2I.get, features)
     for feature in features:
-        vec[feature] += 1
+        if feature != None:
+            vec[feature] += 1
+
     # Should return a numpy vector of features.
     return vec
 
 def accuracy_on_dataset(dataset, params):
     good = bad = 0.0
     for label, features in dataset:
+        x = feats_to_vec(features)
+        y_pred = ll.predict(x, params)
+        if y_pred == L2I.get(label):
+            good += 1
+        else:
+            bad += 1
         # YOUR CODE HERE
         # Compute the accuracy (a scalar) of the current parameters
         # on the dataset.
         # accuracy is (correct_predictions / all_predictions)
-        pass
     return good / (good + bad)
 
 def train_classifier(train_data, dev_data, num_iterations, learning_rate, params):
@@ -35,14 +46,19 @@ def train_classifier(train_data, dev_data, num_iterations, learning_rate, params
     learning_rate: the learning rate to use.
     params: list of parameters (initial values)
     """
+    W, b = params
     for I in xrange(num_iterations):
         cum_loss = 0.0 # total loss in this iteration.
         random.shuffle(train_data)
         for label, features in train_data:
             x = feats_to_vec(features) # convert features to a vector.
-            y = label                  # convert the label to number if needed.
+            y = L2I.get(label)                  # convert the label to number if needed.
             loss, grads = ll.loss_and_gradients(x,y,params)
             cum_loss += loss
+            gW, gB = grads
+            W -= learning_rate * gW
+            b -= learning_rate * gB
+
             # YOUR CODE HERE
             # update the parameters according to the gradients
             # and the learning rate.
@@ -59,7 +75,10 @@ if __name__ == '__main__':
     # and call train_classifier.
     
     # ...
+    in_dim = VOCAB_SIZE
+    out_dim = CATEGORIES
    
+
     params = ll.create_classifier(in_dim, out_dim)
-    trained_params = train_classifier(train_data, dev_data, num_iterations, learning_rate, params)
+    trained_params = train_classifier(TRAIN, DEV, num_iterations, learning_rate, params)
 

code/utils.py
@@ -1,6 +1,8 @@
 # This file provides code which you may or may not find helpful.
 # Use it if you want, or ignore it.
 import random
+import numpy as np 
+
 def read_data(fname):
     data = []
     for line in file(fname):
@@ -11,8 +13,8 @@ def read_data(fname):
 def text_to_bigrams(text):
     return ["%s%s" % (c1,c2) for c1,c2 in zip(text,text[1:])]
 
-def one_hot_vector(vec):
-    out = np.zeros(len(vec))
+def one_hot_vector(vec, length):
+    out = np.zeros(length)
     out[vec] = 1
     return out
 