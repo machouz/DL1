e54e7f2cdc3c7901e93d85d8c5432b302b2cf7a1
arie.cattan <arie.cattan@gmail.com>
2018-11-04 23:11:12 +0200

    implement mlpn add a function hidden_layers which returns the y_pred and 2 vectors: aggregation: the input of each hidden layer activation: the output of each hidden layer after tanh

--- code/mlpn.py
+++ code/mlpn.py
@@ -1,14 +1,40 @@
 import numpy as np
+from loglinear import softmax
+from utils import *
 
 STUDENT={'name': 'cattana_',
          'ID': '336319314_F957022'}
 
 def classifier_output(x, params):
     # YOUR CODE HERE.
+    out = x
+    layer_number = len(params)
+    for i in xrange(0, layer_number, 2):
+        hidden = out.dot(params[i]) + params[i + 1]
+        out = np.tanh(hidden)
+
+    probs = softmax(out)
+
     return probs
 
 def predict(x, params):
     return np.argmax(classifier_output(x, params))
+
+def hidden_layers(x, params):
+    aggregation = []
+    activation = [x]
+    out = x
+
+    layer_number = len(params)
+    for i in xrange(0, layer_number, 2):
+        aggr = out.dot(params[i]) + params[i + 1]
+        out = np.tanh(aggr)
+        aggregation.append(aggr)
+        activation.append(out)
+
+    y_pred = softmax(out)
+
+    return y_pred, aggregation, activation
 
 def loss_and_gradients(x, y, params):
     """
@@ -28,7 +54,14 @@ def loss_and_gradients(x, y, params):
     you should not have gW2 and gb2.)
     """
     # YOU CODE HERE
-    return ...
+
+    y_pred, aggregation, activation = hidden_layers(x, params)
+    loss = -np.log(y_pred[y])
+    grads = []
+
+
+
+    return loss, grads
 
 def create_classifier(dims):
     """
@@ -50,6 +83,20 @@ def create_classifier(dims):
     to first layer, then the second two are the matrix and vector from first to
     second layer, and so on.
     """
+    layers_number = len(dims)
     params = []
+
+    for i in xrange(layers_number - 1):
+        eps = np.sqrt(6.0 / (dims[i] + dims[i+1]))
+        W = np.random.uniform(-eps, eps, (dims[i], dims[i+1]))
+        b = np.random.uniform(-eps, eps, dims[i+1])
+        params.append(W)
+        params.append(b)
+
     return params
 
+
+if __name__ == '__main__':
+    params = create_classifier([600, 200, 100, 6])
+    x = np.random.uniform(0, 1, 600)
+    y_pred, aggregation, activation = hidden_layers(x, params)
